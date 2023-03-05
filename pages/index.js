4cf97db3da072d7c9c6a9be2a9bb0834a7188952
unknown <arminfrontend@gmail.com>
2023-03-05 16:00:32 +0330

    feat: Completed nav

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -10,21 +10,36 @@ export default function Home() {
       </Head>
       <main>
         {/* Hedear section  */}
-        <section id='header'>
-            {/* main container  */}
-            <div className='container max-w-7xl p-6 mx-auto'>
-                {/* Nav */}
-                <nav className='flex justify-between items-center'>
-                  <h1 className='text-xl font-burtons'>developedbyArmin</h1>
-                  {/* icon and button container  */}
-                  <div className='flex space-x-8 items-center'>
+        <section id="header">
+          {/* main container  */}
+          <div className="container max-w-7xl p-6 mx-auto">
+            {/* Nav */}
+            <nav className="flex justify-between items-center">
+              <h1 className="text-xl font-burtons">developedbyArmin</h1>
+              {/* icon and button container  */}
+              <div className="flex space-x-8 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 cursor-pointer"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                  />
+                </svg>
+                <a href="#" className="bg-cyan-500 px-4 py-2 rounded-md text-white bg-gradient-to-r
+                 from-cyan-500 to-teal-500">Resume</a>
+              </div>
+            </nav>
 
-                  </div>
-                </nav>
-            </div>
+          </div>
         </section>
-        
       </main>
     </>
-  )
+  );
 }
