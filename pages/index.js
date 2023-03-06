31fd8fac04e50e440fd7e1ceed0ee7a219db9bc6
unknown <arminfrontend@gmail.com>
2023-03-06 23:45:22 +0330

    Feat: Implemented sun/moon icons

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ import dev from "../public/Images/dev-ed-wave.png";
 import design from "../public/Images/design.png";
 import code from "../public/Images/code.png";
 import consulting from "../public/Images/consulting.png";
-import web1 from "../public/Images/web1.png"
-import web2 from "../public/Images/web2.png"
-import web3 from "../public/Images/web3.png"
-import web4 from "../public/Images/web4.png"
-import web5 from "../public/Images/web5.png"
-import web6 from "../public/Images/web6.png"
+import web1 from "../public/Images/web1.png";
+import web2 from "../public/Images/web2.png";
+import web3 from "../public/Images/web3.png";
+import web4 from "../public/Images/web4.png";
+import web5 from "../public/Images/web5.png";
+import web6 from "../public/Images/web6.png";
 
 export default function Home() {
-  const [darkMode , setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark" : ""}>
       <Head>
@@ -31,24 +31,45 @@ export default function Home() {
           <div className="main-container">
             {/* Nav */}
             <nav className="flex justify-between items-center">
-              <h1 className="text-xl font-burton dark:text-white">developedbyArmin</h1>
+              <h1 className="text-xl font-burtons dark:text-white">
+                developedbyArmin
+              </h1>
               {/* icon and button container  */}
               <div className="flex space-x-8 items-center">
-                <svg
-                onClick={() => setDarkMode(!darkMode)}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 cursor-pointer"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-                  />
-                </svg>
+                {darkMode ? (
+                  <svg
+                  onClick={() => setDarkMode(!darkMode)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-white cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    onClick={() => setDarkMode(!darkMode)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                    />
+                  </svg>
+                )}
+
                 <a
                   href="#"
                   className="bg-cyan-500 px-4 py-2 rounded-md text-white bg-gradient-to-r
@@ -65,7 +86,9 @@ export default function Home() {
               <h2 className="text-5xl text-teal-600 py-2 font-medium">
                 Dimitri Marco
               </h2>
-              <h3 className="text-2xl py-2 dark:text-white">Developer and Designer</h3>
+              <h3 className="text-2xl py-2 dark:text-white">
+                Developer and Designer
+              </h3>
               <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">
                 Freelancer providing services for programming and design content
                 needs. join me down below and let's get cracking!
@@ -199,29 +222,52 @@ export default function Home() {
             <div className="flex flex-col lg:flex-row lg:flex-wrap gap-10 py-10">
               {/* Image 1 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web1} alt="web1" className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web1}
+                  alt="web1"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
               {/* Image 2 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web2} alt="web2" className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web2}
+                  alt="web2"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
               {/* Image 3 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web3} alt="web3" className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web3}
+                  alt="web3"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
               {/* Image 4 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web4} alt="web4" className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web4}
+                  alt="web4"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
               {/* Image 5 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web5} alt="web5" className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web5}
+                  alt="web5"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
               {/* Image 6 */}
               <div className="basis-1/3 flex-1">
-                  <Image src={web6}alt="web6"  className="rounded-lg object-cover w-full h-full"/>
+                <Image
+                  src={web6}
+                  alt="web6"
+                  className="rounded-lg object-cover w-full h-full"
+                />
               </div>
-              
             </div>
           </div>
         </section>
